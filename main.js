8910fae2668a5f0b586ec432b07619b863ce2e62
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-03-23 12:38:09 +0100

    add sass

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>Yo yo yo team what UP welcome to this muthaf*** let's get it get it</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
