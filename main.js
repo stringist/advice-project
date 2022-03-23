eeece4f1fa5ab55804797eabd408959ba7c73953
paulafiaschi <paul6399@stud.kea.dk>
2022-03-23 18:13:26 +0100

    hi team

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <h1>Yo yo yo team what UP welcome to this muthaf*** let's get it get it</h1>
+  <h2> Yo yo yo, we are gonna nail it &#x1F60E<h2>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
