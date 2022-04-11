c88109a9b2546b86eb7f34ccde08c019cfad4ec4
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-04-11 11:57:42 +0200

    pretty responsive and created function to change values inside the charts

--- main.js
+++ main.js
@@ -3,16 +3,35 @@ import "./sass/style.scss";
 const key = "AIzaSyAenWpaU3tqIjJc1d2HtM0juCYilYx1gPs";
 const url = encodeURIComponent("https://kea.dk");
 async function getPageSpeedData() {
-    const result = await fetch(
-        `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${key}`
-    );
-    const data = await result.json();
-    console.log(data);
+  const result = await fetch(
+    `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${key}`
+  );
+  const data = await result.json();
+  console.log(data);
 }
 async function getCarbonData() {
-    const result = await fetch(`https://api.websitecarbon.com/site?url=${url}`);
-    const data = await result.json();
-    console.log(data);
+  const result = await fetch(`https://api.websitecarbon.com/site?url=${url}`);
+  const data = await result.json();
+  console.log(data);
 }
 getPageSpeedData();
 getCarbonData();
+
+// ----------------- Pie charts ---------------------------
+
+perfomancePerc(64);
+
+function perfomancePerc(value) {
+  const perfomanceChart = (document.querySelector(
+    ".percent svg circle:nth-child(2)"
+  ).style.strokeDashoffset = pieChartPercentage(value));
+  const percentageOutput = (document.querySelector(
+    "#performance h2"
+  ).textContent = value);
+}
+
+function pieChartPercentage(number) {
+  let numberInPercentage = 440 - (440 * number) / 100;
+
+  return numberInPercentage;
+}
